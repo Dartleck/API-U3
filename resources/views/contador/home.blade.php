d51afcf515a87135b53ad6685db4df042e0b28ec
Dartleck <159466137+Dartleck@users.noreply.github.com>
2024-04-22 19:42:02 -0600

    Resources

---
+++ resources/views/contador/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Inicio del contador</title>
+</head>
+<body>
+    <h1>Bienvenido, contador</h1>
+    <!-- Contenido específico para el Encargado -->
+</body>
+</html>
